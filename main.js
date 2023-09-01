0167da69c64e427dfc876da697aebe4a14b656d5
SantiRubiol <santirubiol14@gmail.com>
2023-09-01 16:54:07 -0300

    Corrección de errores y agrego animación Game Over

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const $btnAmarillo = document.querySelector("#amarillo");
 const $btnVerde = document.querySelector("#verde");
 const $btnStart = document.querySelector("#btn-start");
 const $puntaje = document.querySelector("#puntaje");
+const $lblInfo = document.querySelector(".label-info");
 
 const luces = [
   () => {
@@ -26,6 +27,7 @@ let listaClickUsuario = [];
 let puntaje = 0;
 let nroTurno = 0;
 let indiceTouch = 0;
+let enPartida = false;
 
 function clickColor($componente, colorOriginal, colorBrillante) {
   setTimeout(() => ($componente.style.backgroundColor = colorOriginal), 500);
@@ -50,6 +52,8 @@ $btnVerde.onclick = function () {
 function comprobarIgualdadColor(indiceColor) {
   if (habilitado) {
     const color = luces[indiceColor]();
+    console.log("Color que deberia tocar: " + listaMemoria[indiceTouch]);
+    console.log("Color que toca: " + color);
     if (listaMemoria[indiceTouch] === color) {
       if (indiceTouch < listaMemoria.length - 1) {
         // SI ACIERTA TODOS LOS COLORES
@@ -60,37 +64,35 @@ function comprobarIgualdadColor(indiceColor) {
         nuevoTurno();
       }
     } else {
-      gameOver()
+      $lblInfo.innerHTML = "GAME OVER" + "\n" + "Puntaje: " + puntaje;
+      gameOver();
     }
   }
 }
 
 function gameOver() {
-  
-
   puntaje = 0;
   habilitado = false;
-  animacionGameOver()
+  animacionGameOver();
 }
 
-//
+
 function animacionGameOver() {
   const tiempo_vuelta = 350;
   const cantidad_vueltas = 3;
   for (let j = 0; j < cantidad_vueltas; j++) {
-    let tiempo_1 = 0;
-    setTimeout(() => {
-      for (let i = 0; i < 8; i++) {
-        const a = i % luces.length;
-        setTimeout(() => {
-          luces[a]();
-        }, tiempo_vuelta * i);
-        tiempo_1 = tiempo_1 + tiempo_vuelta * i;
-      }
-    }, tiempo_1 + tiempo_vuelta * 8 * j);
+      let tiempo_1 = 0;
+      setTimeout(() => {
+        for (let i = 0; i < 8; i++) {
+            const a = i % luces.length;
+            setTimeout(() => {
+              luces[a]();
+            }, tiempo_vuelta * i);
+            tiempo_1 = tiempo_1 + tiempo_vuelta * i;
+        }
+      }, tiempo_1 + tiempo_vuelta * 8 * j);
   }
-}
-// animacionGameOver();
+} 
 
 function generarColorAleatorio() {
   const random = Math.trunc(Math.random() * 100) % 4;
@@ -101,7 +103,22 @@ function nuevaPartida() {
   listaMemoria = [];
   nroTurno = 0;
   puntaje = 0;
-  nuevoTurno();
+
+  let segundosRestantes = 3;
+
+  function mostrarContadorEnReversa() {
+    if (segundosRestantes > 0) {
+      $lblInfo.innerHTML = "" + segundosRestantes;
+      segundosRestantes--;
+    } else {
+      $lblInfo.innerHTML = "A JUGAR!";
+      clearInterval(intervalo);
+      nuevoTurno();
+    }
+  }
+
+  // Ejecutar la función cada segundo
+  const intervalo = setInterval(mostrarContadorEnReversa, 1000);
 }
 
 function nuevoTurno() {
@@ -115,7 +132,8 @@ function nuevoTurno() {
     habilitado = true;
   }, 800);
 }
-
+//gameOver();
 $btnStart.onclick = function () {
+  enPartida = true;
   nuevaPartida();
 };
